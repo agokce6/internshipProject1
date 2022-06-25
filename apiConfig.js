bd0b542ae60313ed0c05a0cb98ce517209c4e8e4
agokce6 <46759694+agokce6@users.noreply.github.com>
2022-06-25 21:42:12 +0300

    Update apiConfig.js

--- apiConfig.js
+++ apiConfig.js
@@ -1,12 +1,12 @@
 // https://imdb-api.com/ 
 // since free api-keys have 100 query for a day, i created multiple accounts for testing.
 
-var APIKEYs = [ "k_cg22nqwv",
-                "k_sbjkg4ye", 
-                "k_vyafu6b0", 
-                "k_a4td5ch7", 
-                "k_6c0ldrel", 
-                "k_lspukfez" 
+var APIKEYs = [ "**********",
+                "**********", 
+                "**********", 
+                "**********", 
+                "**********", 
+                "**********" 
             ];
 
 var APIKEY = APIKEYs[0];
